72ae89dcbec4607cce773b3c3d1cdefbb44691c3
0B1t322 <dan.demin@gmail.com>
2023-11-26 17:07:42 +0300

    add marks

--- src/CPU.Emulator/Assembler/Segments/Segment.cs
+++ src/CPU.Emulator/Assembler/Segments/Segment.cs
@@ -4,8 +4,10 @@ namespace CPU.Emulator.Assembler.Segments;
 
 public class Segment
 {
-    public const string SegmentPatter =
-        @"(?<segmentName>.*) segment '(?<segmentType>.*)' begin\n (?<data>[a-z \n;0-9,]*)\nend";
+    public const string SegmentPattern =
+        @"(?<segmentName>.*) segment '(?<segmentType>.*)' begin\n (?<data>[a-z \n;0-9,A-Z:_]*)\nend";
+
+    public const string MarkPattern = @"[A-Za-z_]*:";
 
     public SegmentType Type { get; set; }
     public string Name { get; set; }
@@ -14,9 +16,10 @@ public class Segment
     public static IEnumerable<Segment> ReadAllSegmentsFromFile(string file)
     {
         var segments = new List<Segment>();
-        RegexOptions options = RegexOptions.Multiline;
+        var marks = new Dictionary<string, uint>();
 
-        foreach (Match m in Regex.Matches(file, SegmentPatter, options))
+        foreach (Match m in Regex.Matches(file, SegmentPattern,
+                     RegexOptions.Multiline))
         {
             var name = m.Groups["segmentName"].Value;
             SegmentType type;
@@ -27,8 +30,41 @@ public class Segment
 
             var splits = data.Split('\n')
                 .Select(s => s.Trim())
+                .Select((s, idx) =>
+                {
+                    if (!Regex.IsMatch(s, MarkPattern))
+                    {
+                        return s;
+                    }
+                    var match = Regex.Match(s, MarkPattern);
+
+                    marks.Add(match.Value.Replace(":",""), 
+                        (uint)(idx - marks.Count));
+                    return "";
+                })
                 .ToList();
+
+            splits.RemoveAll(string.IsNullOrWhiteSpace);
+
+            if (marks.Count > 0)
+            {
+                var regex = MarksToRegex(marks);
+                splits = splits
+                    .Select(s =>
+                    {
+                        if (!Regex.IsMatch(s, regex))
+                        {
+                            return s;
+                        }
+                    
+                        var m = Regex.Match(s, regex);
             
+                        var addr = marks[m.Value];
+                        return s.Replace(m.Value, $"0x{Convert.ToString(addr, 16)}");
+                    })
+                    .ToList();   
+            }
+
             segments.Add(new Segment()
             {
                 Type = type,
@@ -38,5 +74,14 @@ public class Segment
         }
 
         return segments;
+    }
+
+    public static string MarksToRegex(Dictionary<string, uint> marks)
+    {
+        var keys = marks
+            .Select(kv => kv.Key)
+            .ToList();
+
+        return $"(?:{string.Join("|", keys)})";
     }
 }
